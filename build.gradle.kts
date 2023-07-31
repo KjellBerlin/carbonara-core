dd9f9c41f431cfa840eb992c39ab663f688064ed
KjellBerlin <38405651+KjellBerlin@users.noreply.github.com>
2023-07-31 19:58:42 +0200

    Fix config and update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.0"
+	id("org.springframework.boot") version "3.1.2"
 	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.8.21"
-	kotlin("plugin.spring") version "1.8.21"
+
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+
+	application
 }
 
 group = "com.carbonara"
