c3e2f0c6bc37138e7f922acc8ed2c36df764fc65
KjellBerlin <38405651+KjellBerlin@users.noreply.github.com>
2024-04-04 00:32:12 +0200

    Protect endpoints

--- src/main/kotlin/com/carbonara/core/security/SecurityConfig.kt
+++ src/main/kotlin/com/carbonara/core/security/SecurityConfig.kt
@@ -1,17 +1,12 @@
 package com.carbonara.core.security
 
 import org.springframework.context.annotation.Bean
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter
 import org.springframework.security.web.server.SecurityWebFilterChain
-import reactor.core.publisher.Flux
 
 @EnableWebFluxSecurity
-@EnableReactiveMethodSecurity
 class SecurityConfig {
 
     @Bean
@@ -22,19 +17,9 @@ class SecurityConfig {
                     .pathMatchers("/graphql").authenticated()
             }
             .oauth2ResourceServer { oauth2 ->
-                oauth2.jwt { jwt ->
-                    jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
-                }
+                oauth2
+                    .jwt(withDefaults())
             }
             .build()
-    }
-
-    private fun jwtAuthenticationConverter(): ReactiveJwtAuthenticationConverter {
-        val converter = ReactiveJwtAuthenticationConverter()
-        converter.setJwtGrantedAuthoritiesConverter { jwt: Jwt ->
-            val scopes = jwt.getClaimAsStringList("scope")
-            Flux.fromIterable(scopes.orEmpty().map { scope -> SimpleGrantedAuthority("SCOPE_$scope") })
-        }
-        return converter
     }
 }
