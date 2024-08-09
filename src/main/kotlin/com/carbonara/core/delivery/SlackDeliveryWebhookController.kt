7c95b21b634ed92dfaa9cab5d3499cc8ba23221a
KjellBerlin <kjell.lillie-stolze@ampero.ai>
2024-08-09 13:22:21 +0200

    Slack webhook String request body

--- src/main/kotlin/com/carbonara/core/delivery/SlackDeliveryWebhookController.kt
+++ src/main/kotlin/com/carbonara/core/delivery/SlackDeliveryWebhookController.kt
@@ -3,6 +3,7 @@ package com.carbonara.core.delivery
 import com.carbonara.core.order.OrderService
 import mu.KotlinLogging
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,8 +13,8 @@ class SlackDeliveryWebhookController(
 
     // Potential dos attack endpoint, introduce rate limiting
 
-    @PostMapping("/slack-delivery-status", "application/x-www-form-urlencoded")
-    suspend fun handleMollieWebhook(requestBody: String) {
+    @PostMapping("/slack-delivery-status", consumes = ["application/json"])
+    suspend fun handleSlackWebhook(@RequestBody requestBody: String) {
         log.info("--Start Slack--")
         log.info(requestBody)
         log.info("--End Slack--")
