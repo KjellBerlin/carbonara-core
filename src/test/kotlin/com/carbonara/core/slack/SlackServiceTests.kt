039a69f4503b60f4cb2afbf6c3070031a07afa3c
KjellBerlin <kjell.lillie-stolze@ampero.ai>
2024-09-23 11:16:10 +0200

    Fix tests

--- src/test/kotlin/com/carbonara/core/slack/SlackServiceTests.kt
+++ src/test/kotlin/com/carbonara/core/slack/SlackServiceTests.kt
@@ -31,7 +31,7 @@ class SlackServiceTests {
         OrderStatusUpdateScenario("delivered", OrderStatus.DELIVERED),
         OrderStatusUpdateScenario("cancelled", OrderStatus.CANCELLED)
     ).map { scenario ->
-        DynamicTest.dynamicTest("Happy case for order status update with status ${scenario.orderType}") {
+        DynamicTest.dynamicTest("Happy case for order status update with status ${scenario.orderStatus}") {
             val orderDao = createOrderDao(orderStatus = scenario.expectedOrderStatus)
             val slackMessageParams = SlackMessageParams(
                 customerName = orderDao.userName,
@@ -40,14 +40,20 @@ class SlackServiceTests {
                 googleMapsLink = orderDao.deliveryAddress.createGoogleMapsLink(),
                 productNames = orderDao.products.map { it.productName },
                 orderStatus = scenario.expectedOrderStatus,
-                timeStamp = "1726842841"
+                timeStamp = "1726842841",
+                userName = "sherlock.holmes"
             )
 
             coEvery { orderService.updateOrderStatus(any(), any()) } returns orderDao
             coEvery { slackMessageService.updateOrderMessage(any()) } returns Unit
 
             runBlocking {
-                slackService.handleOrderStatusUpdate(orderDao.orderId.toString(), scenario.orderType, "1726842841")
+                slackService.handleOrderStatusUpdate(
+                    orderId = orderDao.orderId.toString(),
+                    slackOrderStatus = scenario.orderStatus,
+                    messageTimestamp = "1726842841",
+                    userName = "sherlock.holmes"
+                )
             }
 
             coVerify { orderService.updateOrderStatus(orderDao.orderId.toString(), scenario.expectedOrderStatus) }
@@ -57,6 +63,6 @@ class SlackServiceTests {
 }
 
 data class OrderStatusUpdateScenario(
-    val orderType: String,
+    val orderStatus: String,
     val expectedOrderStatus: OrderStatus
 )
