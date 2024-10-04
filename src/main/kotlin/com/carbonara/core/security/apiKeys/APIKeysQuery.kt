7232d559fda5f83c7662162e757e560a41f2ea0d
KjellBerlin <kjell.lillie-stolze@ampero.ai>
2024-10-04 18:11:34 +0200

    Remove auth0 props from apiKey endpoint

--- src/main/kotlin/com/carbonara/core/security/apiKeys/APIKeysQuery.kt
+++ src/main/kotlin/com/carbonara/core/security/apiKeys/APIKeysQuery.kt
@@ -11,18 +11,10 @@ class APIKeysQuery : Query {
     @Value("\${google.apiKey}")
     lateinit var googlePlacesAPIKey: String
 
-    @Value("\${okta.oauth2.issuer}")
-    lateinit var auth0Domain: String
-
-    @Value("\${okta.oauth2.client-id}")
-    lateinit var auth0ClientId: String
-
     @GraphQLDescription("APIKeys a user needs to use third party APIs")
     suspend fun apiKeys(): APIKeyDto {
         return APIKeyDto(
-            googlePlacesAPIKey = googlePlacesAPIKey,
-            auth0Domain = auth0Domain,
-            auth0ClientId = auth0ClientId
+            googlePlacesAPIKey = googlePlacesAPIKey
         )
     }
 }
